2082eed844c05b4e5f26e5de74b9a009416a9f95
Jeppe Vinkel Beier <jevb@gfforsikring.dk>
2022-12-05 14:10:56 +0100

    Working sort of

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-window.renderBrowse = (containerId, history) => {
-  ReactDOM.render(
-    <App history={history} />,
-    document.getElementById(containerId),
-  );
-  // unregister();
-};
+window.Widget = {
+  render: (containerId) => {
+    window.Widget.root = ReactDOM.createRoot(document.getElementById(containerId))
+    window.Widget.root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  },
+  unmount: () => {
+    window.Widget.root.unmount()
+  }
+}
 
-window.unmountBrowse = containerId => {
-  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
-};
+// window.renderWidget = (containerId, history) => {
+//   const root = ReactDOM.createRoot(document.getElementById(containerId));
+//   root.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+//   // unregister();
+// };
+
+// window.unmountWidget = containerId => {
+//   ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+// };
